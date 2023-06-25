fd0a20ce837efd94ad3fd6a12abc632930abb4d0
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-06-25 16:28:25 +0430

    String Methods

---
+++ String methods.py
@@ -0,0 +1,33 @@
+a = "hello python WORLD"
+# print(a.capitalize())
+# print(a.casefold())
+# x = a.center(2000)
+# x = a.encode()
+# x = a.endswith("orld")
+# a = "h\te\tl\tl\to"
+# x = a.expandtabs(2)
+# x = a.find("w")
+# x = a.isalnum()
+# x = a.isalpha()
+# x = a.isdecimal()
+# x = a.isdigit()
+# x = a.islower()
+# x = a.isnumeric()
+# x = a.isspace()
+# x = a.isprintable()
+# x = a.isupper()
+# x = a.join("banana")
+
+# x = a.ljust(100)
+# print(x,"hello new code :)")
+
+# x = a.lstrip()
+# x = a.rstrip()
+
+# x = a.partition("python")
+# x = a.splitlines()
+# x = a.startswith("hello") 
+# x = a.swapcase()
+# x = a.title()
+
+print(x)
