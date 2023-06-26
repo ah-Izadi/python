67a350eee37e24bfc9541ad3b183770c436f18c5
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-06-26 10:36:48 +0430

    Comparison_Operators

---
+++ Comparison_operators.py
@@ -0,0 +1,13 @@
+#comparison operators
+# == Equal ..... != Not Equal ..... > gearter than ..... < Less than ..... >= Gearter Than Or Equal To .....
+# <= Lesss Than Or Equal To
+
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
