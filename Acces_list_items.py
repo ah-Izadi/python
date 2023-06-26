3031b860ca6024aaaef4695cc43d53640ee7d2a9
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-06-26 11:35:18 +0430

    Acces_list_Items

---
+++ Acces_list_items.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+#first item is in index 0
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1:3])
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-3:-1])
+
+
