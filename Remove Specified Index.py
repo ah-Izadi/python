8983f44b0361a43bfe8477b7544ea8b58c17db0e
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-06-26 16:47:02 +0430

    Remove Specified Index

---
+++ Remove Specified Index.py
@@ -0,0 +1,18 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+#pop() method can remove the last item in list
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#del method also like pop() method
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#del method also can delete the list completely
+thislist = ["apple", "banana", "cherry"]
+del thislist
