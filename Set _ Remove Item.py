1e1477a75d097e0853998eed0b4d69e27fa4e7a1
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-06-27 13:12:31 +0430

    Set_Remove Item

---
+++ Set _ Remove Item.py
@@ -0,0 +1,23 @@
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+#
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+
+#
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
