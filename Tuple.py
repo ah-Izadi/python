f3289fcde57e44ea455ac9e5a848c1c54c01506b
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-06-27 13:13:45 +0430

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,19 @@
+# Create a Tuple:
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# Tuples allow duplicate values:
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+# Tuple Length
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+# One Item Tuple
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
