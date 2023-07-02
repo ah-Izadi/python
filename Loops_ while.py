f3e9a3ca5dc32ed1c91f9b2bf1ab18c4cc15c635
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-07-02 11:53:54 +0430

    Loops_ While

---
+++ Loops_ while.py
@@ -0,0 +1,30 @@
+i = 0
+
+#create While 
+while i < 6 :
+    i += 1
+    print(i)
+    
+#break statment
+# while i < 6 :
+#     i+=1
+#     if i == 3:
+#         break
+#     print(i)
+#I want when i == 3 stop counting and break the while :)
+
+#continue statment
+# while i < 6 : 
+#     i += 1 
+#     if i == 3:
+#         continue
+#     print(i)
+#I want when i == 3 stop the current iteration , and continue with the next 
+
+#else statment 
+# while i < 6 :
+#     print(i)
+#     i += 1
+# else :
+#     print("'i' is no longer less than 6")
+#I want print massage when i no longer less than 6
