c8214be095dbaef1497e0821acc9ed5a017812e2
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-07-05 11:15:37 +0430

    Exam5

---
+++ Exam5.py
@@ -0,0 +1,4 @@
+input_nums = input("enter the numbers that isolated with , : ")
+
+print(list(input_nums.split(",")))
+print(tuple(input_nums.split(",")))
---
+++ TestCode_ Exam5.py
@@ -0,0 +1,7 @@
+def myfunc(input_nums):
+    return(list(input_nums.split(",")))
+assert myfunc("10,20,30,40") == ['10', '20', '30', '40']
+
+def myfunc(input_nums):
+    return(tuple(input_nums.split(",")))
+assert myfunc("10,20,30,40") == ('10', '20', '30', '40')
