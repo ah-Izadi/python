c7aeadd914b9c70875f40ab2dc5225846621c77e
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-07-09 13:44:07 +0430

    iterators

---
+++ learning_files/iterators.py
@@ -0,0 +1,14 @@
+# myList = ['apple','banana','cherry']
+# MyIterator = iter(myList)
+# print(next(MyIterator))
+# print(next(MyIterator))
+# print(next(MyIterator))
+
+myStr = "banana"
+MyIterator = iter(myStr)
+print(next(MyIterator))
+print(next(MyIterator))
+print(next(MyIterator))
+print(next(MyIterator))
+print(next(MyIterator))
+print(next(MyIterator))
