120d441ce4b2c2333dc6a641305018fbb5425dff
amirhossein izadi <a.h.izadi.ir@gmail.com>
2023-07-14 22:20:33 +0430

    Phonebook

--- Phonebook.py
+++ Phonebook.py
@@ -4,23 +4,19 @@ class phone:
     def __init__(self,name,phoneNum):
         self.name = name
         self.phoneNum = phoneNum
-
-class SetPhoneNum(phone):
+        
     def SetPhoneNum(self):
         if not self.phoneNum < 0:
             phoneDict[self.name] = self.phoneNum
-            return phoneDict
-class Find:
-    def __init__(self,Name):
-        self.Name = Name
-        # phone.__init__(self,name,phoneNum)
-    def findPhoneNum(self):
-        if phoneDict[self.Name]:
-            print(phoneDict[self.Name])
-        else:
-            print("this name was not difined !!!!!!!")
+            return(phoneDict)
 
-p1 = SetPhoneNum(input("please enter the name : "),int(input("please enter the PhoneNumber without 0 : ")))
+    def findPhoneNum(self,Name):
+        if phoneDict[Name]:
+            return("0" + str(phoneDict[Name]))
+        else:
+            return("this name was not difined !!!!!!!")
+            
+            
+p1 = phone(input("please enter the name : "),int(input("please enter the PhoneNumber without 0 : ")))
 print(p1.SetPhoneNum())
-find = Find(input("please enter the name that you want find phoneNum :"))
-print(find.findPhoneNum())
+print(p1.findPhoneNum(input("please enter the name : ")))
